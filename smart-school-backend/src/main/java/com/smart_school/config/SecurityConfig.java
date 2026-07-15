94e9fe52f9cc984cf6094ba3b858ba44302b1d39
Jayanthrayudu <jayanthrayudu7.com>
2026-07-15 20:18:50 +0530

    backend changes

--- smart-school-backend/src/main/java/com/smart_school/config/SecurityConfig.java
+++ smart-school-backend/src/main/java/com/smart_school/config/SecurityConfig.java
@@ -62,7 +62,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+        "http://localhost:3000",
+        "https://smart-school-frontend-rk3s.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
